3292fc668f5b506b90e340957f947ca3ed2805ba
Samuele Minissale <minissalesamuele@gmail.com>
2021-11-11 16:43:39 +0100

    add Esercizio 1 JS

--- JavaScript/Esercizio 1 - definire la variabile/exercise-1.js
+++ JavaScript/Esercizio 1 - definire la variabile/exercise-1.js
@@ -2,7 +2,7 @@ function canPlay() {
   
   let personName = 'Paul'; // Se si dichiara una variabile all'interno di un if, essa esisterà solo al suo interno
 
-  personName += ' plays football'; // Argh! personName is not defined
+  personName += ' plays football'; 
 
   console.log(personName);
 }
