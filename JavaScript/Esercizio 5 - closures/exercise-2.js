48932193f24caac000bc8fb012f8c47c4a96a743
Samuele Minissale <minissalesamuele@gmail.com>
2021-11-14 15:01:14 +0100

    Add Esercizio 5 - closures

---
+++ JavaScript/Esercizio 5 - closures/exercise-2.js
@@ -0,0 +1,22 @@
+function pow(base) {
+  return function(exp) {
+    let result=1;
+    for (let index = 0; index < exp; index++) {
+      result*=base;
+    }
+    return result;
+  };
+}
+
+const pow2 = pow(2);
+const pow5 = pow(5);
+
+console.log('--- Power of 2 ---');
+console.log(pow2(4));
+console.log(pow2(2));
+console.log(pow2(8));
+
+console.log('--- Power of 5 ---');
+console.log(pow5(4));
+console.log(pow5(2));
+console.log(pow5(8));
