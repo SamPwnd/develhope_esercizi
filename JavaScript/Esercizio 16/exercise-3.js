4c4e2053321a551421b0ffc33f4d615c4f77a8f4
Samuele Minissale <minissalesamuele@gmail.com>
2021-11-18 18:07:55 +0100

    add Es 16

---
+++ JavaScript/Esercizio 16/exercise-3.js
@@ -0,0 +1,20 @@
+function createStore() {
+  const products = new Array();
+  return (item) => {
+    products.push(item);
+    console.log(products);
+  };
+}
+
+const redPants = { id: 1, name: 'Red Pants' };
+const whiteHat = { id: 2, name: 'White Hat' };
+const blackSneakers = { id: 3, name: 'Black Sneakers' };
+
+console.log('--- Dress Store ---');
+const dressStore = createStore();
+dressStore(redPants);
+dressStore(whiteHat)
+
+console.log('--- Shoes Store ---');
+const shoesStore = createStore();
+shoesStore(blackSneakers);
